191b8c36facbf51839739cbda62c3409ead9f844
SteveStrop <steve_tropical@yahoo.co.uk>
2018-09-09 08:58:03 +0100

    Added login API (byob  end section 11)

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -5,6 +5,7 @@ from . import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')  # displayed in Django API root
 router.register('profile', views.UserProfileViewSet)  # displayed in Django API root (127.0.0.1:8080/api/)
+router.register('login', views.LoginViewSet, base_name='login')
 
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),  # points to an APIView
