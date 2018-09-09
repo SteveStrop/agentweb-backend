d74224107c36f5eab14d4fb29b2acd8f1076479d
SteveStrop <steve_tropical@yahoo.co.uk>
2018-09-09 09:49:12 +0100

    Added improved permissions to use profile feed items API (byob  end 12)

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -5,7 +5,8 @@ from . import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')  # displayed in Django API root
 router.register('profile', views.UserProfileViewSet)  # displayed in Django API root (127.0.0.1:8080/api/)
-router.register('login', views.LoginViewSet, base_name='login')
+router.register('login', views.LoginViewSet, base_name='login') # this is how client will recognise it
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),  # points to an APIView
